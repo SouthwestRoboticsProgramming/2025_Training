e4bfa35539d09340dc4e2edde672871d5734b17e
Mason Vogt <mvog2501@mpsedu.org>
2024-05-09 11:16:09 -0500

    Wipe Mini Bot code
    This lets the rookie programmers learn how to write it starting off with a skelleton.

--- Robot/src/main/java/com/swrobotics/robot/RobotContainer.java
+++ Robot/src/main/java/com/swrobotics/robot/RobotContainer.java
@@ -1,10 +1,7 @@
 package com.swrobotics.robot;
 
-import com.swrobotics.robot.commands.DefaultDriveCommand;
 import com.swrobotics.robot.control.ControlBoard;
 import com.swrobotics.robot.logging.Logging;
-import com.swrobotics.robot.subsystems.Drive;
-import com.swrobotics.robot.subsystems.Pincher;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.*;
@@ -16,8 +13,7 @@ public class RobotContainer {
 //    public static final Logging.SimMode SIM_MODE = Logging.SimMode.REPLAY;
 
     public final LoggedPowerDistribution pdp;
-    public final Drive drive;
-    public final Pincher pincher;
+    // Put new subsystems here
 
     public final ControlBoard controlboard;
 
@@ -26,13 +22,10 @@ public class RobotContainer {
         DriverStation.silenceJoystickConnectionWarning(RobotBase.isSimulation());
 
         pdp = LoggedPowerDistribution.getInstance(63, PowerDistribution.ModuleType.kCTRE); // FIXME: Set ID
-        drive = new Drive();
-        pincher = new Pincher();
 
         // ControlBoard must be initialized last
         controlboard = new ControlBoard(this);
 
-        drive.setDefaultCommand(new DefaultDriveCommand(drive, () -> controlboard.getDriveForward(), () -> controlboard.getDriveTurn()));
     }
 
     public Command getAutonomousCommand() {
--- Robot/src/main/java/com/swrobotics/robot/control/ControlBoard.java
+++ Robot/src/main/java/com/swrobotics/robot/control/ControlBoard.java
@@ -9,22 +9,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public final class ControlBoard extends SubsystemBase {
 
-    public final XboxController controller;
+    // Add an XboxController here
 
     public ControlBoard(RobotContainer robot) {
-        // Passing deadband here means we don't have to deadband anywhere else
-        controller = new XboxController(0, 0.2);
 
-        new Trigger(() -> controller.a.isPressed())
-            .onTrue(runOnce(() -> robot.pincher.set(true)))
-            .onFalse(runOnce(() -> robot.pincher.set(false)));
-    }
-
-    public double getDriveForward() {
-        return -MathUtil.powerWithSign(controller.getLeftStick().getY(), 2); // Might be positive
-    }
-
-    public double getDriveTurn() {
-        return -MathUtil.powerWithSign(controller.getLeftStick().getX(), 2);
     }
 }
